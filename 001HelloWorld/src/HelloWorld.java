72e3bd2747fdc6412b87f31702191b11efd74967
Bruno Soares <brunohenrique.soares@outlook.com>
2021-05-30 20:11:41 -0300

    Primeiro exercício de Java
    Adicionando o primeiro exercício de Java, apenas mostrando no console o "Hello World".

---
+++ 001HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author bruno
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        //Comando printando a tela
+		System.out.println("Hello World com JAVA");
+		System.out.println("Testando o básico de java ;-;");
+    }
+}
